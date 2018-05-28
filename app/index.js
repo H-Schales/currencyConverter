901c512c4a9df6fbd34ac2d4e4442844266a259a
Hendrik Schales <hendrik.schales@sovanta.com>
2018-05-28 13:33:02 +0200

    added the Header

--- app/index.js
+++ app/index.js
@@ -9,6 +9,8 @@ EStyleSheet.build({
     $border: '#E2E2E2',
     $inputText: '#797979',
     $lightGray: '#F0F0F0',
+
+    //$outline: 1, --> show outline in Simulator...helps to debug the Views
 });
 
 export default () => <Home />;
