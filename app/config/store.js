211cf2ddcf7361c041c9f5bb2b2de02d42141053
Hendrik Schales <hendrik.schales@sovanta.com>
2018-07-22 18:28:50 +0200

    basic saga setup

--- app/config/store.js
+++ app/config/store.js
@@ -1,19 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-// import createSagaMiddleware from "redux-saga";
+import createSagaMiddleware from "redux-saga";
 
 import reducer from "../reducers";
-// import rootSaga from "./sagas";
+import rootSaga from "./sagas";
 
-// const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware]; //middleware just for Development
-//Because never ship this to production
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 if (process.env.NODE_ENV === "development") {
   middleware.push(logger);
 }
 
 const store = createStore(reducer, applyMiddleware(...middleware));
 
-// sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
 
 export default store;
